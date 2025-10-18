b177c64d8823349afe1f850bc0f24c50e254e5da
ivanmg10 <ivanmg10.outlook.es>
2025-10-18 22:42:08 +0200

    feat: add basics error and loading pages

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+export default async function Loading() {
+  return (
+    <main>
+      <section className="flex flex-col items-center justify-center h-screen gap-5">
+        <h1 className="text-5xl font-bold">Cargando...</h1>
+      </section>
+    </main>
+  );
+}
